9bd06c201893c037e0d4f5775f26c8714e5e1a8f
Zaion Felippe <81268185+ZaionKun@users.noreply.github.com>
2022-04-02 19:13:35 -0300

    Add files via upload

---
+++ LISTA ENCADEADA/lista.py
@@ -0,0 +1,11 @@
+#criando uma funçao para inicialização da linkedLista
+from platform import node
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+# Modo interativo para testar a aplicação acima - "python3"
+# 
